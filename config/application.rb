cda626fd087ea395cb76d408ab7c697806a64ec2
soar <85624529@qq.com>
2014-10-02 11:28:22 +0800

    add rspec

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,39 @@ module City
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.i18n.default_locale = :en
+    config.i18n.fallbacks = [:en]
+    config.i18n.available_locales = [:en, :"zh-CN"]
+    config.i18n.enforce_available_locales = false
+
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.test_framework :rspec,
+        :view_specs    => false,
+        :controller_specs => false,
+        :routing_specs => false
+    end
+
+    config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+    config.assets.precompile += Ckeditor.assets
+   #config.assets.precompile += %w(error_code.css error_code.js information.css information2.css information.js information2.js iscroll.css chat.css chat.js)
+    #config.autoload_paths += Dir[Rails.root.join('app', 'entities', '*')]
+    #config.autoload_paths += Dir[Rails.root.join('app', 'apis', '*')]
+
+    config.action_controller.asset_host = Settings.asset_host
+
+    config.active_record.default_timezone = :local
+    config.time_zone = 'Beijing'
+=begin
+
+    config.middleware.insert_before "Rack::Runtime", "Rack::Cors" do
+      allow do
+      origins '*'
+      resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
+=end
+
   end
 end
