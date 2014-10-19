3d74b67ba40783d01a805be20a1b9928aaa16114
soar <85624529@qq.com>
2014-10-19 17:13:28 +0800

    add information tag

--- app/models/information.rb
+++ app/models/information.rb
@@ -12,6 +12,12 @@ class Information < ActiveRecord::Base
       self.publish == true ? "是" : "否"
    end
 
+   def own_by_shop?
+       self.owner_type == 'Shop'
+   end
+  def own_by_user?
+       self.owner_type == 'User'
+   end
    private
       def contact_at_least_one
           if  !mobile_number.present? && !qq.present? && !weixin.present?
