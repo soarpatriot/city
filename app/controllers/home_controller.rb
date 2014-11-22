af2ac1d4b8f3ff08b5e19e7049248b4b5eb98be7
Soar <85624529@qq.com>
2014-11-22 21:55:35 +0800

    add about page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,6 +11,7 @@ class HomeController < ApplicationController
    def feedback
 
      FeedbackMailer.feed(current_user, params[:content]).deliver
-     redirect_to about_path
+
+     redirect_to about_path, notice: '非常感谢您的宝贵意见。'
    end
 end
