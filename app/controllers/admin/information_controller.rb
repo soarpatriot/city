368df8265c09813c4fcad11caa3939b59c88d3e9
Soar <85624529@qq.com>
2014-11-23 09:26:13 +0800

    add admin edit the category

--- app/controllers/admin/information_controller.rb
+++ app/controllers/admin/information_controller.rb
@@ -1,9 +1,22 @@
 class Admin::InformationController < Admin::AdminController
 
-  before_action :set_information, only: [:show, :destroy]
+  before_action :set_information, only: [:show, :destroy, :edit, :update]
 
   def index
     @information = Information.page params[:page]
+  end
+
+  def edit
+
+  end
+
+  # PATCH/PUT /information/1
+  def update
+    if @information.update(information_params)
+      redirect_to admin_information_index_path, notice: '信息更新成功'
+    else
+      render :edit
+    end
   end
 
    # DELETE /information/1
@@ -23,6 +36,6 @@ class Admin::InformationController < Admin::AdminController
 
   # Only allow a trusted parameter "white list" through.
   def information_params
-    params.require(:information).permit(:id, :title, :description, :publish,:image, :price, :contact, :mobile_number, :qq, :weixin)
+    params.require(:information).permit(:id, :title, :description, :publish,:image, :price, :contact, :mobile_number, :qq, :weixin, :category_id)
   end
 end
