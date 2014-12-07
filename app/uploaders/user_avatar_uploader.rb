3f690e64653a42029a68b3bc8503179339924834
Soar <85624529@qq.com>
2014-12-07 17:49:02 +0800

    fixed the comment error

--- app/uploaders/user_avatar_uploader.rb
+++ app/uploaders/user_avatar_uploader.rb
@@ -7,7 +7,7 @@ class UserAvatarUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
   include CarrierWave::MiniMagick
 
-  process :resize_to_fit => [800, 2048]
+  process :resize_to_fit => [800, 800]
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
@@ -33,7 +33,7 @@ class UserAvatarUploader < CarrierWave::Uploader::Base
   #   # do something
   # end
   version :small do
-     process :resize_to_fit => [360, 2048]
+     process :resize_to_fill => [360, 360]
     # process :resize_to_fit => [230, nil]
     # process :store_dimensions
     # process crop: '300x150+0+0'
@@ -41,7 +41,7 @@ class UserAvatarUploader < CarrierWave::Uploader::Base
   end
 
   version :thumb, :from_version => :small do
-      process resize_to_fit: [80, 2048]
+      process resize_to_fill: [80, 80]
   end
   
   def extension_white_list
