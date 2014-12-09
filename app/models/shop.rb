09b342fa28619d3fa2f7ce49f22ed9393a1ebfc0
Soar <85624529@qq.com>
2014-12-09 23:29:16 +0800

    add shop subdomain information

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -2,8 +2,10 @@ class Shop < ActiveRecord::Base
 
   VALID_URL_KEY_REGEX = /\A[a-z\d\-_]+\z/
   validates :title, :description, :address, :contact, :phone,presence:true
-
+  validates_exclusion_of :subdomain, :in => %w( support blog www billing help api test ), :message => "此关键字 <strong>{{value}}</strong> 是保留关键字，不可用，请更换其它."
   validates :url, format: { with: VALID_URL_KEY_REGEX, message: "输入格式不正确" }, uniqueness: { case_sensitive: false }
+
+  before_validation :downcase_subdomain
 
   belongs_to :user
   has_many :information, as: :owner, dependent: :destroy
@@ -12,7 +14,14 @@ class Shop < ActiveRecord::Base
 
   def access_url
      unless self.url.blank?
-         "http://#{self.url}.#{Settings.domain}"
+       "http://#{self.url}.#{Settings.domain}"
+     else
+       shop_url self
      end
   end
+
+  protected
+    def downcase_subdomain
+      self.url.downcase! if attribute_present?("url")
+    end
 end
