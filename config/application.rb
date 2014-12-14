a2726f72ed4bafb9de8befea1c693e6573a02a4b
Soar <85624529@qq.com>
2014-12-14 21:11:49 +0800

    change domain allow across

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -47,6 +48,11 @@ module City
     config.time_zone = 'Beijing'
 
 
+
+    config.action_dispatch.default_headers.merge!({
+                                                      'Access-Control-Allow-Origin' => '*',
+                                                      'Access-Control-Request-Method' => '*'
+                                                  })
 
   end
 end
