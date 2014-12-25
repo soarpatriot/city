79e4992ee3ac6c82b719e3f92ad3f8f01e58beb4
Soar <85624529@qq.com>
2014-12-25 12:56:40 +0800

    add human file size helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def human_file_size bytes_size
+    kb_size = bytes_size / 1024.0
+    if kb_size < 1024
+      return sprintf("%.2f", kb_size) + " kb"
+    else
+      mb_size = kb_size / 1024.0
+      return sprintf("%.2f", mb_size) + " mb"
+    end
+  end
 end
